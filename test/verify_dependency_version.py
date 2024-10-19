4a17c71153b5b02b897487f00f6ca6d66d6319ce
Andrade <vinicius_andrade2010@hotmail.com>
2024-10-19 20:40:53 -0300

    refactor: main.py

---
+++ test/verify_dependency_version.py
@@ -0,0 +1,19 @@
+import importlib.metadata
+
+pacotes = [
+    'python-dotenv',
+    'boto3',
+    'awscli',
+    'openai',
+    'python-docx',
+    'reportlab',
+    'PyPDF2',
+    'ebooklib'
+]
+
+for nome_pacote in pacotes:
+    try:
+        versao = importlib.metadata.version(nome_pacote)
+        print(f"{nome_pacote}: {versao}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{nome_pacote}: Não está instalado")
